0a2c95e4f73946b8c1b004c8f2af33333f0fcec8
Carlos <delamorenacoco.carlos@gmail.com>
2021-01-07 14:23:37 +0300

    optimizando el testeo de la grafica

--- grafica.py
+++ grafica.py
@@ -5,5 +5,6 @@ print("TF version:", tf.__version__)
 print("TF Hub version:", hub.__version__)
 # Check for GPU availability
 print("GPU", "available (YESSSS!!!!!)" if tf.config.list_physical_devices("GPU") else "not available :(")
-
+physical_devices = tf.config.experimental.list_physical_devices("GPU")
+tf.config.experimental.set_memory_growth(physical_devices[0], True)
 print("Eager mode: ", tf.executing_eagerly())
